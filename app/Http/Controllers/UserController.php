8e4a012df0fe5aeec5ebbd636b992ce129e52c1d
motinMTN <luigi.motin@gmail.com>
2022-09-01 20:50:31 -0500

    added age calculation per user from Backend

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,12 +5,17 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use \Illuminate\Http\Response;
+use Illuminate\Support\Carbon;
 
 class UserController extends Controller
 {
     public function users()
     {
-        $usuarios = User::with('domicilios')->get();
+        $usuarios = User::with('domicilios')->get()->each(function ($user)
+        {
+            $edad = Carbon::parse($user->fecha_nacimiento)->age;
+            $user->edad = $edad;
+        });
         $array = $usuarios->toArray();
 
         return response()->json($array,Response::HTTP_OK);
